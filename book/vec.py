2c4b54a7361e1629c93e326e721df1669f999e9c
Steven Yang <yangchenyun@gmail.com>
2023-03-22 12:10:42 -0700

    implemented book/vec.py for later use

--- book/vec.py
+++ book/vec.py
@@ -1,6 +1,7 @@
 # Copyright 2013 Philip N. Klein
 
-def getitem(v,k):
+
+def getitem(v, k):
     """
     Return the value of entry k in v.
     Be sure getitem(v,k) returns 0 if k is not represented in v.f.
@@ -12,9 +13,13 @@ def getitem(v,k):
     0
     """
     assert k in v.D
-    pass
+    if k in v.f:
+        return v.f[k]
+    else:
+        return 0
 
-def setitem(v,k,val):
+
+def setitem(v, k, val):
     """
     Set the element of v with label d to be val.
     setitem(v,d,val) should set the value for key d even if d
@@ -32,9 +37,10 @@ def setitem(v,k,val):
     0
     """
     assert k in v.D
-    pass
+    v.f[k] = val
 
-def equal(u,v):
+
+def equal(u, v):
     """
     Return true iff u is equal to v.
     Because of sparse representation, it is not enough to compare dictionaries
@@ -68,12 +74,13 @@ def equal(u,v):
     False
     """
     assert u.D == v.D
-    pass
+    return all(u[d] == v[d] for d in u.D)
 
-def add(u,v):
+
+def add(u, v):
     """
     Returns the sum of the two vectors.
-    
+
     Consider using brackets notation u[...] and v[...] in your procedure
     to access entries of the input vectors.  This avoids some sparsity bugs.
 
@@ -105,9 +112,10 @@ def add(u,v):
     True
     """
     assert u.D == v.D
-    pass
+    return Vec(u.D, {d: v[d] + u[d] for d in u.D})
 
-def dot(u,v):
+
+def dot(u, v):
     """
     Returns the dot product of the two vectors.
 
@@ -139,7 +147,8 @@ def dot(u,v):
     12
     """
     assert u.D == v.D
-    pass
+    return sum(u[d] * v[d] for d in u.D)
+
 
 def scalar_mul(v, alpha):
     """
@@ -159,7 +168,8 @@ def scalar_mul(v, alpha):
     >>> u == Vec({'x','y','z','w'},{'x':1,'y':2,'z':3,'w':4})
     True
     """
-    pass
+    return Vec(v.D, {d: alpha * v[d] for d in v.D})
+
 
 def neg(v):
     """
@@ -176,9 +186,11 @@ def neg(v):
     >>> -Vec({'a','b','c'}, {'a':1}) == Vec({'a','b','c'}, {'a':-1})
     True
     """
-    pass
+    return Vec(v.D, {d: -v[d] for d in v.D})
+
 
 ###############################################################################################################################
+
 
 class Vec:
     """
@@ -187,6 +199,7 @@ class Vec:
     f - a dictionary mapping (some) domain elements to field elements
         elements of D not appearing in f are implicitly mapped to zero
     """
+
     def __init__(self, labels, function):
         assert isinstance(labels, set)
         assert isinstance(function, dict)
@@ -196,17 +209,17 @@ class Vec:
     __getitem__ = getitem
     __setitem__ = setitem
     __neg__ = neg
-    __rmul__ = scalar_mul #if left arg of * is primitive, assume it's a scalar
+    __rmul__ = scalar_mul  # if left arg of * is primitive, assume it's a scalar
 
-    def __mul__(self,other):
-        #If other is a vector, returns the dot product of self and other
+    def __mul__(self, other):
+        # If other is a vector, returns the dot product of self and other
         if isinstance(other, Vec):
-            return dot(self,other)
+            return dot(self, other)
         else:
             return NotImplemented  #  Will cause other.__rmul__(self) to be invoked
 
-    def __truediv__(self,other):  # Scalar division
-        return (1/other)*self
+    def __truediv__(self, other):  # Scalar division
+        return (1 / other) * self
 
     __add__ = add
 
@@ -215,9 +228,9 @@ class Vec:
         if other == 0:
             return self
 
-    def __sub__(a,b):
+    def __sub__(a, b):
         "Returns a vector which is the difference of a and b."
-        return a+(-b)
+        return a + (-b)
 
     __eq__ = equal
 
@@ -225,26 +238,41 @@ class Vec:
         s = 0
         for x in self.f.values():
             if isinstance(x, int) or isinstance(x, float):
-                s += x*x
+                s += x * x
             elif isinstance(x, complex):
                 y = abs(x)
-                s += y*y
-            else: return False
+                s += y * y
+            else:
+                return False
         return s < 1e-20
 
     def __str__(v):
         "pretty-printing"
         D_list = sorted(v.D, key=repr)
         numdec = 3
-        wd = dict([(k,(1+max(len(str(k)), len('{0:.{1}G}'.format(v[k], numdec))))) if isinstance(v[k], int) or isinstance(v[k], float) else (k,(1+max(len(str(k)), len(str(v[k]))))) for k in D_list])
-        s1 = ''.join(['{0:>{1}}'.format(str(k),wd[k]) for k in D_list])
-        s2 = ''.join(['{0:>{1}.{2}G}'.format(v[k],wd[k],numdec) if isinstance(v[k], int) or isinstance(v[k], float) else '{0:>{1}}'.format(v[k], wd[k]) for k in D_list])
-        return "\n" + s1 + "\n" + '-'*sum(wd.values()) +"\n" + s2
+        wd = dict(
+            [
+                (k, (1 + max(len(str(k)), len("{0:.{1}G}".format(v[k], numdec)))))
+                if isinstance(v[k], int) or isinstance(v[k], float)
+                else (k, (1 + max(len(str(k)), len(str(v[k])))))
+                for k in D_list
+            ]
+        )
+        s1 = "".join(["{0:>{1}}".format(str(k), wd[k]) for k in D_list])
+        s2 = "".join(
+            [
+                "{0:>{1}.{2}G}".format(v[k], wd[k], numdec)
+                if isinstance(v[k], int) or isinstance(v[k], float)
+                else "{0:>{1}}".format(v[k], wd[k])
+                for k in D_list
+            ]
+        )
+        return "\n" + s1 + "\n" + "-" * sum(wd.values()) + "\n" + s2
 
     def __hash__(self):
         "Here we pretend Vecs are immutable so we can form sets of them"
         h = hash(frozenset(self.D))
-        for k,v in sorted(self.f.items(), key = lambda x:repr(x[0])):
+        for k, v in sorted(self.f.items(), key=lambda x: repr(x[0])):
             if v != 0:
                 h = hash((h, hash(v)))
         return h
@@ -257,4 +285,4 @@ class Vec:
         return Vec(self.D, self.f.copy())
 
     def __iter__(self):
-        raise TypeError('%r object is not iterable' % self.__class__.__name__)
+        raise TypeError("%r object is not iterable" % self.__class__.__name__)
