f28a84339c30881c0e560a4ba9457da6127cc61f
Steven Yang <yangchenyun@gmail.com>
2023-05-13 02:12:53 -0700

    Implement all scalar/element-wise operator

--- src/ndarray_backend_cpu.cc
+++ src/ndarray_backend_cpu.cc
@@ -195,7 +195,65 @@ void ScalarAdd(const AlignedArray& a, scalar_t val, AlignedArray* out) {
 
 /// BEGIN YOUR SOLUTION
 
+#define EWISE_BINARY_OP(OPNAME, OP) \
+void Ewise##OPNAME(const AlignedArray& a, const AlignedArray& b, AlignedArray* out) { \
+  for (size_t i = 0; i < a.size; i++) { \
+    out->ptr[i] = a.ptr[i] OP b.ptr[i]; \
+  } \
+}
+
+#define SCALAR_BINARY_OP(OPNAME, OP) \
+void Scalar##OPNAME(const AlignedArray& a, scalar_t val, AlignedArray* out) { \
+  for (size_t i = 0; i < a.size; i++) { \
+    out->ptr[i] = a.ptr[i] OP val; \
+  } \
+}
+
+#define EWISE_UNARY_OP(OPNAME, OP) \
+void Ewise##OPNAME(const AlignedArray& a, AlignedArray* out) { \
+  for (size_t i = 0; i < a.size; i++) { \
+    out->ptr[i] = OP(a.ptr[i]); \
+  } \
+}
+
+#define SCALAR_UNARY_OP(OPNAME, OP) \
+void Scalar##OPNAME(const AlignedArray& a, AlignedArray* out) { \
+  for (size_t i = 0; i < a.size; i++) { \
+    out->ptr[i] = OP(a.ptr[i]); \
+  } \
+}
+
+
+SCALAR_BINARY_OP(Mul, *)
+SCALAR_BINARY_OP(Div, /)
+SCALAR_BINARY_OP(Eq, ==)
+SCALAR_BINARY_OP(Ge, >=)
+void ScalarMaximum(const AlignedArray& a, scalar_t val, AlignedArray* out) {
+  for (size_t i = 0; i < a.size; i++) {
+    out->ptr[i] = std::max(a.ptr[i], val);
+  }
+}
+void ScalarPower(const AlignedArray& a, scalar_t val, AlignedArray* out) {
+  for (size_t i = 0; i < a.size; i++) {
+    out->ptr[i] = pow(a.ptr[i], val);
+  }
+}
+
+EWISE_BINARY_OP(Mul, *)
+EWISE_BINARY_OP(Div, /)
+EWISE_BINARY_OP(Eq, ==)
+EWISE_BINARY_OP(Ge, >=)
+EWISE_UNARY_OP(Log, log)
+EWISE_UNARY_OP(Exp, exp)
+EWISE_UNARY_OP(Tanh, tanh)
+void EwiseMaximum(const AlignedArray& a, const AlignedArray& o, AlignedArray* out) {
+  for (size_t i = 0; i < a.size; i++) {
+    out->ptr[i] = std::max(a.ptr[i], o.ptr[i]);
+  }
+}
+
 /// END YOUR SOLUTION
+
 
 void Matmul(const AlignedArray& a, const AlignedArray& b, AlignedArray* out, uint32_t m, uint32_t n,
             uint32_t p) {
@@ -341,22 +399,22 @@ PYBIND11_MODULE(ndarray_backend_cpu, m) {
   m.def("ewise_add", EwiseAdd);
   m.def("scalar_add", ScalarAdd);
 
-  // m.def("ewise_mul", EwiseMul);
-  // m.def("scalar_mul", ScalarMul);
-  // m.def("ewise_div", EwiseDiv);
-  // m.def("scalar_div", ScalarDiv);
-  // m.def("scalar_power", ScalarPower);
+  m.def("ewise_mul", EwiseMul);
+  m.def("scalar_mul", ScalarMul);
+  m.def("ewise_div", EwiseDiv);
+  m.def("scalar_div", ScalarDiv);
+  m.def("scalar_power", ScalarPower);
 
-  // m.def("ewise_maximum", EwiseMaximum);
-  // m.def("scalar_maximum", ScalarMaximum);
-  // m.def("ewise_eq", EwiseEq);
-  // m.def("scalar_eq", ScalarEq);
-  // m.def("ewise_ge", EwiseGe);
-  // m.def("scalar_ge", ScalarGe);
+  m.def("ewise_maximum", EwiseMaximum);
+  m.def("scalar_maximum", ScalarMaximum);
+  m.def("ewise_eq", EwiseEq);
+  m.def("scalar_eq", ScalarEq);
+  m.def("ewise_ge", EwiseGe);
+  m.def("scalar_ge", ScalarGe);
 
-  // m.def("ewise_log", EwiseLog);
-  // m.def("ewise_exp", EwiseExp);
-  // m.def("ewise_tanh", EwiseTanh);
+  m.def("ewise_log", EwiseLog);
+  m.def("ewise_exp", EwiseExp);
+  m.def("ewise_tanh", EwiseTanh);
 
   m.def("matmul", Matmul);
   m.def("matmul_tiled", MatmulTiled);
