0827a72a18f3ccc9ee3519744ccd7492908a9564
Steven Yang <yangchenyun@gmail.com>
2023-05-19 22:21:39 -0700

    init for CNN weights

--- python/needle/init.py
+++ python/needle/init.py
@@ -78,20 +78,24 @@ def xavier_normal(fan_in, fan_out, gain=1.0, **kwargs):
     return randn(fan_in, fan_out, std=std, **kwargs)
 
 
-def kaiming_uniform(fan_in, fan_out, nonlinearity="relu", **kwargs):
+def kaiming_uniform(fan_in, fan_out, shape=None, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
+    if shape is None:
+        shape = (fan_in, fan_out)
     gains = {
         'relu': math.sqrt(2)
     }
     bound = gains[nonlinearity] * math.sqrt(3 / (fan_in))
-    return rand(fan_in, fan_out, low=-bound, high=bound, **kwargs)
+    return rand(*shape, low=-bound, high=bound, **kwargs)
 
 
-def kaiming_normal(fan_in, fan_out, nonlinearity="relu", **kwargs):
+def kaiming_normal(fan_in, fan_out, shape, nonlinearity="relu", **kwargs):
     assert nonlinearity == "relu", "Only relu supported currently"
+    if shape is None:
+        shape = (fan_in, fan_out)
     gains = {
         'relu': math.sqrt(2)
     }
     gain = gains[nonlinearity] 
     std = gain / math.sqrt(fan_in)
-    return randn(fan_in, fan_out, std=std, **kwargs)
+    return randn(*shape, std=std, **kwargs)
